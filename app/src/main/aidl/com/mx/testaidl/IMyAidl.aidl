5dfabd407e447908b898f1e7f577b7a4e18a6d8f
liuhe <liuhe@meixing.com>
2019-01-02 18:09:31 +0800

    Initial commit

---
+++ app/src/main/aidl/com/mx/testaidl/IMyAidl.aidl
@@ -0,0 +1,13 @@
+// IMyAidl.aidl
+package com.mx.testaidl;
+
+// Declare any non-default types here with import statements
+
+interface IMyAidl {
+    /**
+     * Demonstrates some basic types that you can use as parameters
+     * and return values in AIDL.
+     */
+    void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
+            double aDouble, String aString);
+}
---
+++ app/src/main/java/com/mx/testaidl/bean/Person.java
@@ -0,0 +1,45 @@
+package com.mx.testaidl.bean;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * @author: lhe
+ * @date: 2019/01/02
+ * @desctiption: xxxx
+ */
+public class Person implements Parcelable {
+
+    private String mName;
+
+    public Person(String name) {
+        mName = name;
+    }
+
+
+    protected Person(Parcel in) {
+        mName = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mName);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Person> CREATOR = new Creator<Person>() {
+        @Override
+        public Person createFromParcel(Parcel in) {
+            return new Person(in);
+        }
+
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
+}
